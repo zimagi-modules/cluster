ebf36b366e6dd53b4ff59d0ad91b386e29ceec46
Adrian Webb <adrian@webb.sh>
2019-03-21 02:35:17 -0400

    Updating all references to Config and Manager class names/namespaces.

--- settings/cluster.py
+++ settings/cluster.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from utility.config import Config
+from .config import Config
 
 #-------------------------------------------------------------------------------
 # Core Django settings
