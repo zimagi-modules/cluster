f4b9936318738337d58ae19bc3f88b6b3c13b0bf
Adrian Webb <adrian@webb.sh>
2019-03-21 11:59:09 -0400

    Adding non verbose flag on the terraform release wget command in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@
 # Install cluster management related packaging
 #
 TERRAFORM_VERSION=0.12.0-alpha4
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+wget -nv https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
     && unzip -o terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
     && rm -f terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip
