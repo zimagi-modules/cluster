fc211671f0ef8a164101f58b7a3bb016b2f8f8ab
Adrian Webb <adrian@webb.sh>
2019-03-21 07:29:11 -0400

    Adding cluster management related installation requirements.

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Install cluster management related packaging
+#
+TERRAFORM_VERSION=0.12.0-alpha4
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+    && unzip terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+    && rm -f terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip
