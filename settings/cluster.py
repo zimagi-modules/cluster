0f1530bff55ec551043a83efbe80bf8778004409
Adrian Webb <adrian@webb.sh>
2019-03-22 05:42:10 -0400

    Fixing location of Config class in the cluster settings.

--- settings/cluster.py
+++ settings/cluster.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from .config import Config
+from .settings import Config
 
 #-------------------------------------------------------------------------------
 # Core Django settings
