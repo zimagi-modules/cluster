5471f45cfea6dfebcc1cfafcdecf550baae54321
Adrian Webb <adrian@webb.sh>
2019-03-22 02:44:21 -0400

    Updating the Terraform version and install process in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,8 @@
 #
 # Install cluster management related packaging
 #
-TERRAFORM_VERSION=0.12.0-alpha4
-wget -nv https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
-    && unzip -o terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
-    && rm -f terraform_${TERRAFORM_VERSION}_terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+set -e
+
+TERRAFORM_VERSION=0.12.0-beta1
+wget -nv https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin
