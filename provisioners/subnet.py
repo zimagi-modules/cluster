d9deed3d8306862336a1784b647d6c72b92ad388
Adrian Webb <adrian@webb.sh>
2019-03-29 18:17:58 -0400

    Removing describe method and adding scope and variables methods with network and group_names in the subnet provisioner.

--- provisioners/subnet.py
+++ provisioners/subnet.py
@@ -6,7 +6,6 @@ class Provisioner(profile.BaseProvisioner):
     def priority(self):
         return 3
 
-
     def ensure(self, name, config):
         networks = self.profile.pop_values('network', config)
         groups = self.profile.pop_values('group_names', config)
@@ -23,6 +22,10 @@ class Provisioner(profile.BaseProvisioner):
 
         self.command.run_list(networks, process)
 
+    def scope(self, instance):
+        return { 'network': instance.network.name }
 
-    def describe(self, subnet):
-        return { 'network': subnet.network.name }
+    def variables(self, instance):
+        return {
+            'group_names': [ x.name for x in instance.groups.all() ]
+        }
