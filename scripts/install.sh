7a44e87e5f63c2fa74157b5a4451f25238f91ce6
Adrian Webb <adrian@webb.sh>
2019-03-30 17:26:23 -0400

    Temporarily adding dev Terraform aws plugin provider in the install script until a compatible version is released.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,3 +7,7 @@ set -e
 TERRAFORM_VERSION=0.12.0-beta1
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip 2>/dev/null
 unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin
+
+mkdir -p ~/.terraform.d/plugins
+wget http://terraform-0.12.0-dev-snapshots.s3-website-us-west-2.amazonaws.com/terraform-provider-aws/1.60.0-dev20190216H00-dev/terraform-provider-aws_1.60.0-dev20190216H00-dev_linux_amd64.zip 2>/dev/null
+unzip -o terraform-provider-aws_1.60.0-dev20190216H00-dev_linux_amd64.zip -d ~/.terraform.d/plugins
