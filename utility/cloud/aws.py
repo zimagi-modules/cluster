5fdfae41d8687a75002ac8853b64d29f7f59a6e9
Adrian Webb <adrian@webb.sh>
2019-03-30 15:46:42 -0400

    Eliminating extra whitespace in the aws cloud utility library.

--- utility/cloud/aws.py
+++ utility/cloud/aws.py
@@ -7,12 +7,12 @@ class AWSServiceMixin(object):
         try:
             config['access_key'] = self.command.get_config('aws_access_key', required = True).strip()
             config['secret_key'] = self.command.get_config('aws_secret_key', required = True).strip()
-    
+
         except Exception:
             self.command.error("To use AWS provider you must have 'aws_access_key' and 'aws_secret_key' environment configurations; see: config set")
-        
+
         return config
-    
+
     def clean_aws_credentials(self, config):
         config.pop('access_key', None)
         config.pop('secret_key', None)
@@ -24,17 +24,17 @@ class AWSServiceMixin(object):
             self.session = boto3.Session(
                 aws_access_key_id = config['access_key'],
                 aws_secret_access_key = config['secret_key']
-            )  
+            )
 
     def ec2(self, network):
         self._init_session()
-        return self.session.client('ec2', 
+        return self.session.client('ec2',
             region_name = network.config['region']
         )
 
     def efs(self, network):
         self._init_session()
-        return self.session.client('efs', 
+        return self.session.client('efs',
             region_name = network.config['region']
         )
 
@@ -44,6 +44,6 @@ class AWSServiceMixin(object):
         sgroups = []
 
         for firewall in firewalls:
-            sgroups.append(firewall.variables['security_group_id'])                    
-                
+            sgroups.append(firewall.variables['security_group_id'])
+
         return sgroups
