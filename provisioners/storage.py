490f9226e72bd708f3196180d5b5c0089d31091d
Adrian Webb <adrian@webb.sh>
2019-03-31 11:48:18 -0400

    Adding an initial storage provisioner.

---
+++ provisioners/storage.py
@@ -0,0 +1,47 @@
+from systems.command import profile
+
+
+class Provisioner(profile.BaseProvisioner):
+
+    def priority(self):
+        return 3
+
+    def ensure(self, name, config):
+        provider = self.pop_value('provider', config)
+        networks = self.pop_values('network', config)
+        groups = self.pop_values('groups', config)
+
+        if not providers or not networks:
+            self.command.error("Storage {} requires 'provider' and 'network' fields".format(name))
+
+        def process(network):
+            self.exec('storage save',
+                storage_provider_name = provider,
+                storage_name = name,
+                storage_fields = self.interpolate(config,
+                    network = network
+                ),
+                network_name = network,
+                group_names = groups,
+                test = self.test
+            )
+        self.run_list(networks, process)
+
+    def scope(self, instance):
+        return { 'network': instance.network.name }
+
+    def variables(self, instance):
+        return {
+            'groups': self.get_names(instance.groups)
+        }
+
+    def destroy(self, name, config):
+        networks = self.pop_values('network', config)
+
+        def process_network(network):
+            self.exec('storage rm',
+                storage_name = name,
+                network_name = network,
+                force = True
+            )
+        self.run_list(networks, process_network)
