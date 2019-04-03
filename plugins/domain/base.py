1747599a4dc90eaf2516301e25253aca6b83a744
Adrian Webb <adrian@webb.sh>
2019-04-03 03:20:59 -0400

    Adding an initial base domain meta plugin provider.

---
+++ plugins/domain/base.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+
+from systems.plugins import meta, terraform
+
+
+class DomainProvider(terraform.TerraformPluginProvider):
+
+    def provider_config(self, type = None):
+        self.option(str, 'region', 'us-east-1', help = 'AWS service region')
+
+    def terraform_type(self):
+        return 'domain'
+
+    @property
+    def facade(self):
+        return self.command._domain
+
+
+class DomainRecordProvider(terraform.TerraformPluginProvider):
+
+    def provider_config(self, type = None):
+        self.option(str, 'target', None, help = 'DNS record target domain name')
+        self.option(str, 'type', 'A', help = 'DNS record type')
+        self.option(int, 'ttl', 300, help = 'DNS time to live (secs)')
+        self.option(list, 'values', [], help = 'DNS record values')
+
+    def terraform_type(self):
+        return 'domain_record'
+
+    @property
+    def facade(self):
+        return self.command._domain_record
+
+
+class BaseProvider(meta.MetaPluginProvider):
+
+    def register_types(self):
+        self.set('domain', DomainProvider)
+        self.set('domain_record', DomainRecordProvider)
