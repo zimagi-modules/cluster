d957c96cde043a9925635af867f4967ee9e5f787
Adrian Webb <adrian@webb.sh>
2019-04-03 04:34:08 -0400

    Adding an initial certificate command data mixin.

---
+++ systems/command/mixins/certificate.py
@@ -0,0 +1,16 @@
+from data.certificate.models import Certificate
+from .base import DataMixin
+
+
+class CertificateMixin(DataMixin):
+
+    schema = {
+        'certificate': {
+            'model': Certificate,
+            'provider': True
+        }
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.facade_index['01_certificate'] = self._certificate
