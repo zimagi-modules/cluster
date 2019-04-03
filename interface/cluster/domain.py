61a70ad1927d935f7dc545815aa1eee4f5901494
Adrian Webb <adrian@webb.sh>
2019-04-03 03:27:49 -0400

    Adding an initial domain command set.

---
+++ interface/cluster/domain.py
@@ -0,0 +1,25 @@
+from systems.command.base import command_list
+from systems.command.factory import resource, router
+from systems.command.types import domain
+
+
+class Command(domain.DomainRouterCommand):
+
+    def get_subcommands(self):
+        domain_record_name = 'domain_record'
+
+        return command_list(
+            resource.ResourceCommandSet(
+                domain.DomainActionCommand, self.name,
+                provider_name = self.name,
+                provider_subtype = self.name
+            ),
+            ('record', router.Router(
+                domain.DomainRouterCommand,
+                resource.ResourceCommandSet(
+                    domain.DomainActionCommand, domain_record_name,
+                    provider_name = self.name,
+                    provider_subtype = domain_record_name
+                )
+            ))
+        )
