0044e435db48a4f7f34202f670810d27a7c94079
Adrian Webb <adrian@webb.sh>
2019-04-03 03:23:10 -0400

    Adding an initial AWS Route53 zone domain manifest.

---
+++ terraform/domain/route53.tf
@@ -0,0 +1,21 @@
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_route53_zone" "main" {
+  name = "${var.name}"
+  force_destroy = true
+
+  tags = {
+    Environment = "cenv-domain"
+  }
+}
+output "zone_id" {
+  value = "${aws_route53_zone.main.id}"
+}
+output "zone_name_servers" {
+  value = "${aws_route53_zone.main.name_servers}"
+}
