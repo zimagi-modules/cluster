01b654b77524b524baf9cc400255dea3ceac9672
Adrian Webb <adrian@webb.sh>
2019-04-03 03:21:59 -0400

    Adding an initial AWS Route53 domain meta provider.

---
+++ plugins/domain/route53.py
@@ -0,0 +1,40 @@
+from utility.cloud.aws import AWSServiceMixin
+from .base import *
+
+
+class AWSDomainProvider(AWSServiceMixin, DomainProvider):
+
+    def initialize_terraform(self, instance, created):
+        self.aws_credentials(instance.config)
+        super().initialize_terraform(instance, created)
+
+    def finalize_terraform(self, instance):
+        self.aws_credentials(instance.config)
+        super().finalize_terraform(instance)
+
+    def prepare_instance(self, instance, created):
+        super().prepare_instance(instance, created)
+        self.clean_aws_credentials(instance.config)
+
+
+class AWSDomainRecordProvider(AWSServiceMixin, DomainRecordProvider):
+
+    def initialize_terraform(self, instance, created):
+        self.aws_credentials(instance.config)
+        super().initialize_terraform(instance, created)
+
+    def finalize_terraform(self, instance):
+        self.aws_credentials(instance.config)
+        super().finalize_terraform(instance)
+
+    def prepare_instance(self, instance, created):
+        super().prepare_instance(instance, created)
+        self.clean_aws_credentials(instance.config)
+
+
+class Provider(BaseProvider):
+
+    def register_types(self):
+        super().register_types()
+        self.set('domain', AWSDomainProvider)
+        self.set('domain_record', AWSDomainRecordProvider)
