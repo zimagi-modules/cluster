743dbc9ec91ea02675340865a62bdcba0b43f6a7
Adrian Webb <adrian@webb.sh>
2019-04-03 04:33:37 -0400

    Adding an initial aws terraform certificate manifest.

---
+++ terraform/certificate/aws.tf
@@ -0,0 +1,22 @@
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key      = "${var.private_key}"
+  certificate_body = "${var.certificate}"
+  certificate_chain = "${var.chain}"
+
+  tags = {
+    Name = "cenv-certificate"
+  }
+}
+output "cert_id" {
+  value = "${aws_acm_certificate.cert.id}"
+}
+output "cert_arn" {
+  value = "${aws_acm_certificate.cert.arn}"
+}
