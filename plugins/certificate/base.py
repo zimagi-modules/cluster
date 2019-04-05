324b023b016006fbff768c599fa03c6b49f6a29a
Adrian Webb <adrian@webb.sh>
2019-04-05 15:32:59 -0400

    Removing uneeded provider options and adding domain certificate linking in the base certificate provider.

--- plugins/certificate/base.py
+++ plugins/certificate/base.py
@@ -3,14 +3,17 @@ from systems.plugins import terraform
 
 class BaseProvider(terraform.TerraformPluginProvider):
 
-    def provider_config(self, type = None):
-        self.requirement(str, 'private_key', help = 'PEM formatted private key of certificate')
-        self.requirement(str, 'certificate', help = 'PEM formatted public key of certificate')
-        self.option(str, 'chain', None, help = 'PEM formatted certificate chain')
-
     def terraform_type(self):
         return 'certificate'
 
     @property
     def facade(self):
         return self.command._certificate
+
+    def initialize_terraform(self, instance, created):
+        if instance.domain:
+            instance.private_key = instance.domain.private_key
+            instance.certificate = instance.domain.certificate
+            instance.chain = instance.domain.fullchain
+
+        super().initialize_terraform(instance, created)
