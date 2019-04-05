f944b377d5d61fad15bddef95935c187d907a40c
Adrian Webb <adrian@webb.sh>
2019-04-05 00:42:49 -0400

    Adding certification related fields to the domain model and adding display methods in the model facade.

--- data/domain/models.py
+++ data/domain/models.py
@@ -1,6 +1,6 @@
 from django.db import models as django
 
-from systems.models import environment, group, provider
+from systems.models import environment, group, provider, fields
 
 
 class DomainFacade(
@@ -8,7 +8,17 @@ class DomainFacade(
     group.GroupModelFacadeMixin,
     environment.EnvironmentModelFacadeMixin
 ):
-    pass
+    def get_field_private_key_display(self, instance, value, short):
+        return self.encrypted_color(value)
+
+    def get_field_certificate_display(self, instance, value, short):
+        return self.encrypted_color(value)
+
+    def get_field_chain_display(self, instance, value, short):
+        return self.encrypted_color(value)
+
+    def get_field_fullchain_display(self, instance, value, short):
+        return self.encrypted_color(value)
 
 
 class Domain(
@@ -16,6 +26,16 @@ class Domain(
     group.GroupMixin,
     environment.EnvironmentModel
 ):
+    email = django.CharField(null = True, max_length = 255)
+
+    certificate_authority = django.CharField(null = True, max_length = 255)
+    certificate_updated = django.DateTimeField(null = True, editable = False)
+
+    private_key = fields.EncryptedDataField(null = True)
+    certificate = fields.EncryptedDataField(null = True)
+    chain = fields.EncryptedDataField(null = True)
+    fullchain = fields.EncryptedDataField(null = True)
+
     class Meta(environment.EnvironmentModel.Meta):
         verbose_name = "domain"
         verbose_name_plural = "domains"
