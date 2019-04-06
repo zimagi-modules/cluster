edc4932aaa7363c56f70f478e4ee2ebff0959f84
Adrian Webb <adrian@webb.sh>
2019-04-06 18:22:21 -0400

    Adding update and remove domain dns reference for load balancers in the base load balancer plugin.

--- plugins/load_balancer/base.py
+++ plugins/load_balancer/base.py
@@ -10,6 +10,60 @@ class LoadBalancerProvider(terraform.TerraformPluginProvider):
     def facade(self):
         return self.command._load_balancer
 
+    def prepare_instance(self, instance, created):
+        super().prepare_instance(instance, created)
+        self.update_domain_reference(instance)
+
+    def finalize_terraform(self, instance):
+        self.remove_domain_reference(instance)
+        super().finalize_terraform(instance)
+
+
+    def update_domain_reference(self, instance):
+        if instance.domain:
+            dns_name = instance.variables['lb_dns_name']
+            domain_target = "{}.{}".format(
+                instance.name,
+                instance.domain.name
+            )
+            record = self.command._domain_record.retrieve(
+                domain_target,
+                domain = instance.domain
+            )
+            if not record:
+                provider = self.command.get_provider(
+                    self.command._domain_record.meta.provider_name,
+                    instance.domain.provider_type
+                )
+                record = provider.create(domain_target, {
+                    'domain': instance.domain,
+                    'target': domain_target,
+                    'type': 'CNAME',
+                    'values': [
+                        dns_name
+                    ]
+                })
+            else:
+                if dns_name not in record.values:
+                    record.values.append(dns_name)
+                    record.save()
+
+    def remove_domain_reference(self, instance):
+        if instance.domain:
+            dns_name = instance.variables['lb_dns_name']
+            domain_target = "{}.{}".format(
+                instance.name,
+                instance.domain.name
+            )
+            record = self.command._domain_record.retrieve(
+                domain_target,
+                domain = instance.domain
+            )
+            if record:
+                if dns_name in record.values:
+                    record.values.remove(dns_name)
+                    record.save()
+
 
 class LoadBalancerListenerProvider(terraform.TerraformPluginProvider):
 
