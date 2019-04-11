254f06d793d4789f12b512c4279c3a6f1a13ffb8
Adrian Webb <adrian@webb.sh>
2019-04-11 07:53:16 -0400

    Updating the remote command task provider implementation.

--- plugins/task/remote_command.py
+++ plugins/task/remote_command.py
@@ -1,9 +1,5 @@
-from django.conf import settings
-
 from .base import BaseProvider
 from .mixins import cli, ssh
-
-import copy
 
 
 class Provider(
@@ -11,25 +7,26 @@ class Provider(
     ssh.SSHTaskMixin,
     BaseProvider
 ):
-    def execute(self, results, main_params):
-        servers = main_params.pop('servers', [])
-
+    def execute(self, results, params):
         def exec_server(server):
-            params = copy.deepcopy(main_params)
-
             if 'command' in self.config:
                 command = self.config['command']
             else:
-                self.command.error("Command task provider must have a 'command' property specified")
+                self.command.error("Remote command task provider must have a 'command' property specified")
 
             sudo = self.config.get('sudo', False)
             lock = self.config.get('lock', False)
-            args = self.config.get('args', [])
-            options = self.config.get('options', {})
-            options['_separator'] = self.config.get('option_seperator', ' ')
+            options = self._merge_options(self.config.get('options', {}), params, lock)
 
-            self._parse_args(args, params.pop('args', None), lock)
-            self._parse_options(options, params, lock)
-            self._ssh_exec(server, command, args, options, sudo = sudo)
+            command = self._interpolate(command, options)
+            if sudo:
+                command = 'sudo ' + command[0]
+            else:
+                command = command[0]
 
-        self.command.run_list(servers, exec_server)
+            self._ssh_exec(server, command, sudo = sudo)
+
+        self.command.run_list(
+            self._ssh_servers(params),
+            exec_server
+        )
