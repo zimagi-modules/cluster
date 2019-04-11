7dfa105d1483ac804be5506246c977b9b375c221
Adrian Webb <adrian@webb.sh>
2019-04-11 07:53:55 -0400

    Updating the remote script task plugin provider implementation.

--- plugins/task/remote_script.py
+++ plugins/task/remote_script.py
@@ -1,10 +1,8 @@
-from django.conf import settings
-
+from utility.data import ensure_list
 from .base import BaseProvider
 from .mixins import cli, ssh
 
 import os
-import copy
 
 
 class Provider(
@@ -12,38 +10,36 @@ class Provider(
     ssh.SSHTaskMixin,
     BaseProvider
 ):
-    def execute(self, results, main_params):
-        servers = main_params.pop('servers', [])
-
+    def execute(self, results, params):
         def exec_server(server):
-            params = copy.deepcopy(main_params)
-
             if 'script' in self.config:
                 script_path = self.get_path(self.config['script'])
             else:
-                self.command.error("Script task provider must have a 'script' property specified that links to an executable file")
+                self.command.error("Remote script task provider must have a 'script' property specified that links to an executable file")
 
             if not os.path.exists(script_path):
-                self.command.error("Script task provider file {} does not exist".format(script_path))
+                self.command.error("Remote script task provider file {} does not exist".format(script_path))
 
             script_base, script_ext = os.path.splitext(script_path)
             temp_path = "/tmp/{}{}".format(self.generate_name(24), script_ext)
 
+            sudo = self.config.get('sudo', False)
             lock = self.config.get('lock', False)
-            args = self.config.get('args', [])
-            options = self.config.get('options', {})
-            options['_separator'] = self.config.get('option_seperator', ' ')
+            options = self._merge_options(self.config.get('options', {}), params, lock)
+            args = ensure_list(self.config.get('args', []))
 
             ssh = server.provider.ssh()
-            ssh.upload(script_path, temp_path, mode = 0o700)
+            ssh.upload(script_path, temp_path, mode = 0o755)
             try:
-                self._parse_args(args, params.pop('args', None), lock)
-                self._parse_options(options, params, lock)
-                self._ssh_exec(server, temp_path, args, options,
-                    sudo = self.config.get('sudo', False),
+                self._ssh_exec(server, temp_path,
+                    self._interpolate(args, options),
+                    sudo = sudo,
                     ssh = ssh
                 )
             finally:
                 ssh.sudo('rm -f', temp_path)
 
-        self.command.run_list(servers, exec_server)
+        self.command.run_list(
+            self._ssh_servers(params),
+            exec_server
+        )
