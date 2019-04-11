98b713279dccac5702da03d90f7789bf82a05467
Adrian Webb <adrian@webb.sh>
2019-04-11 07:50:30 -0400

    Removing the ssh method from the server command data mixin.

--- systems/command/mixins/server.py
+++ systems/command/mixins/server.py
@@ -14,18 +14,3 @@ class ServerMixin(NetworkMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.facade_index['03_server'] = self._server
-
-
-    def ssh(self, server, timeout = 10, port = 22):
-        if isinstance(server, str):
-            server = self.get_instance(self._server, server)
-
-        if not server:
-            return None
-
-        return super().ssh(
-            "{}:{}".format(server.ip, port), server.user,
-            password = server.password,
-            key = server.private_key,
-            timeout = timeout
-        )
