d73435d5ac20a141fad74d4885a2b43906d85fe0
Adrian Webb <adrian@webb.sh>
2019-04-11 07:52:38 -0400

    Adding a _ssh_servers method in the ssh task plugin mixin.

--- plugins/task/mixins/ssh.py
+++ plugins/task/mixins/ssh.py
@@ -1,6 +1,14 @@
 
 class SSHTaskMixin(object):
 
+    def _ssh_servers(self, params):
+        return self.command.search_instances(
+            self.manager.get_facade_index()['server'],
+            queries = params.get('servers', []),
+            joiner = params.get('filter', 'AND'),
+            error_on_empty = False
+        )
+
     def _ssh_exec(self, server, command, args = [], options = {}, sudo = False, ssh = None):
         if not ssh:
             ssh = server.provider.ssh()
