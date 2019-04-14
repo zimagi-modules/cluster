1654bf8bf09b32a977278a572255397371d9e75c
Adrian Webb <adrian@webb.sh>
2019-04-14 00:44:48 -0400

    Adding network peering and network relations in the network command data mixin.

--- systems/command/mixins/network.py
+++ systems/command/mixins/network.py
@@ -1,4 +1,6 @@
 from data.network.models import Network
+from data.network_peering.models import NetworkPeering
+from data.network_peering_relation.models import NetworkPeeringRelation
 from data.subnet.models import Subnet
 from data.firewall.models import Firewall
 from data.firewall_rule.models import FirewallRule
@@ -11,6 +13,13 @@ class NetworkMixin(DataMixin):
         'network': {
             'model': Network,
             'provider': True
+        },
+        'network_peering': {
+            'model': NetworkPeering,
+            'provider': True
+        },
+        'network_relation': {
+            'model': NetworkPeeringRelation
         },
         'subnet': {
             'model': Subnet
@@ -26,6 +35,8 @@ class NetworkMixin(DataMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.facade_index['01_network'] = self._network
+        self.facade_index['02_network_peering'] = self._network_peering
+        self.facade_index['03_network_relation'] = self._network_relation
         self.facade_index['02_subnet'] = self._subnet
         self.facade_index['02_firewall'] = self._firewall
         self.facade_index['03_firewall_rule'] = self._firewall_rule
