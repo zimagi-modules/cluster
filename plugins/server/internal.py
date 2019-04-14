b5245cec053bf8250799b2385417135ae240992d
Adrian Webb <adrian@webb.sh>
2019-04-14 01:02:16 -0400

    Removing initialize_instance method override in the internal server provider.

--- plugins/server/internal.py
+++ plugins/server/internal.py
@@ -10,10 +10,3 @@ class Provider(BaseProvider):
 
         self.option(str, 'user', 'admin', help = 'Server SSH user', config_name = 'internal_user')
         self.option(str, 'data_device', '/dev/sda4', help = 'Server data drive device', config_name = 'internal_data_device')
-
-
-    def initialize_instance(self, instance, created):
-        if instance.subnet.network.provider_type != 'internal':
-            self.command.error("Internally defined network needed to create manual server entries")
-
-        super().initialize_instance(instance, created)
