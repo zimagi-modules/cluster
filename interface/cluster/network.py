9cdca8f73756b191e3dde43854bf7ac6a67beb72
Adrian Webb <adrian@webb.sh>
2019-04-14 00:45:36 -0400

    Adding peering command set to the network command interface.

--- interface/cluster/network.py
+++ interface/cluster/network.py
@@ -1,12 +1,25 @@
-from systems.command.factory import resource
+from systems.command.base import command_list
+from systems.command.factory import resource, router
 from systems.command.types import network
 
 
 class Command(network.NetworkRouterCommand):
 
     def get_subcommands(self):
-        return resource.ResourceCommandSet(
-            network.NetworkActionCommand, self.name,
-            provider_name = self.name,
-            provider_subtype = self.name
+        network_peering_name = 'network_peering'
+
+        return command_list(
+            resource.ResourceCommandSet(
+                network.NetworkActionCommand, self.name,
+                provider_name = self.name,
+                provider_subtype = self.name
+            ),
+            ('peering', router.Router(
+                network.NetworkRouterCommand,
+                resource.ResourceCommandSet(
+                    network.NetworkActionCommand, network_peering_name,
+                    provider_name = network_peering_name,
+                    provider_subtype = network_peering_name
+                )
+            ))
         )
