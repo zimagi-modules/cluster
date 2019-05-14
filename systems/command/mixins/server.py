5da9335fb7bbe0313c29f16f9bfa647b056fe945
Adrian Webb <adrian@webb.sh>
2019-05-14 03:34:49 -0400

    Adding the server volume model to the server command data mixin.

--- systems/command/mixins/server.py
+++ systems/command/mixins/server.py
@@ -1,4 +1,5 @@
 from data.server.models import Server
+from data.server_volume.models import ServerVolume
 from .network import NetworkMixin
 
 
@@ -8,9 +9,14 @@ class ServerMixin(NetworkMixin):
         'server': {
             'model': Server,
             'provider': True
+        },
+        'server_volume': {
+            'model': ServerVolume,
+            'provider': True
         }
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.facade_index['03_server'] = self._server
+        self.facade_index['04_server_volume'] = self._server_volume
