0eadb893ee875610d090858f9d23e176f4cf247e
Adrian Webb <adrian@webb.sh>
2019-05-20 01:04:30 -0400

    Using AWS EIP public addresses in the server EC2 Terraform manifests.

--- terraform/server/ec2.tf
+++ terraform/server/ec2.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "server" {
   tenancy = "${var.tenancy}"
   ebs_optimized = "${var.ebs_optimized}"
   monitoring = "${var.monitoring}"
-  associate_public_ip_address = "${var.use_public_ip}"
+  associate_public_ip_address = false
 
   key_name = "${var.key_name}"
   vpc_security_group_ids = "${var.security_groups}"
@@ -28,6 +28,12 @@ resource "aws_instance" "server" {
     Name = "cenv-compute"
   }
 }
+resource "aws_eip" "server_ip" {
+  count = "${var.use_public_ip ? 1 : 0}"
+  instance = "${aws_instance.server.id}"
+  vpc = true
+}
+
 output "instance_id" {
   value = "${aws_instance.server.id}"
 }
@@ -35,5 +41,8 @@ output "private_ip_address" {
   value = "${aws_instance.server.private_ip}"
 }
 output "public_ip_address" {
-  value = "${aws_instance.server.public_ip}"
+  value = "${var.use_public_ip ? aws_eip.server_ip.0.public_ip : null}"
+}
+output "public_dns" {
+  value = "${var.use_public_ip ? aws_eip.server_ip.0.public_dns : null}"
 }
