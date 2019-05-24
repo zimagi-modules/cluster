0e517146c766a56804b7e4003101b25d68404078
Adrian Webb <adrian@webb.sh>
2019-05-24 09:18:47 -0400

    Updating an error message in the AWS cloud utility mixin.

--- utility/cloud/aws.py
+++ utility/cloud/aws.py
@@ -13,7 +13,7 @@ class AWSServiceMixin(object):
             os.environ['AWS_SECRET_ACCESS_KEY'] = config['secret_key']
 
         except Exception:
-            self.command.error("To use AWS provider you must have 'aws_access_key' and 'aws_secret_key' environment configurations; see: config set")
+            self.command.error("To use AWS provider you must have 'aws_access_key' and 'aws_secret_key' environment configurations; see: config save")
 
         return config
 
