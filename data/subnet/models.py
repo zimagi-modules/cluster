7d90649055de2deb9b9776531a766481c3fcc3d2
Adrian Webb <adrian@webb.sh>
2019-05-26 10:58:10 -0400

    Adding use_public_ip, use_net, and nat_subnets fields to the subnet model.

--- data/subnet/models.py
+++ data/subnet/models.py
@@ -11,6 +11,10 @@ class SubnetFacade(
     def get_field_cidr_display(self, instance, value, short):
         return value
 
+    def get_field_nat_subnets_display(self, instance, value, short):
+        subnets = [ str(x) for x in value.all() ]
+        return "\n".join(subnets)
+
 
 class Subnet(
     provider.ProviderMixin,
@@ -18,6 +22,14 @@ class Subnet(
     network.NetworkModel
 ):
     cidr = django.CharField(null = True, max_length = 128)
+
+    use_public_ip = django.BooleanField(default = True)
+    use_nat = django.BooleanField(default = False)
+
+    nat_subnets = django.ManyToManyField("Subnet",
+        related_name = "%(class)s_relations",
+        editable = False
+    )
 
     class Meta(network.NetworkModel.Meta):
         verbose_name = "subnet"
