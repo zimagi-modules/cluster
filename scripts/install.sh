bf099d2844485742ec914141e07aa3e4bacab6c9
Adrian Webb <adrian@webb.sh>
2020-01-22 14:11:04 -0500

    Updating the Terraform module installation script commands and removing zip files after installation.

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,11 @@ rm -rf /var/lib/apt/lists/*
 TERRAFORM_VERSION=0.12.19
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip 2>/dev/null
 unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin
+rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
 mkdir -p ~/.terraform.d/plugins
-wget http://http://terraform-0.12.0-dev-snapshots.s3-website-us-west-2.amazonaws.com/terraform-provider-aws/2.7.0-dev20190415H16-dev/terraform-provider-aws_2.7.0-dev20190415H16-dev_linux_amd64.zip 2>/dev/null
-unzip -o terraform-provider-aws_2.7.0-dev20190415H16-dev_linux_amd64.zip -d ~/.terraform.d/plugins
+
+TERRAFORM_AWS_VERSION=2.45.0
+wget https://releases.hashicorp.com/terraform-provider-aws/${TERRAFORM_AWS_VERSION}/terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip 2>/dev/null
+unzip -o terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip -d ~/.terraform.d/plugins
+rm -f terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip
