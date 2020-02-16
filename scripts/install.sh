de195393b46a3a39af38ec4a3620517725e8fc39
Adrian Webb <adrian@webb.sh>
2020-02-16 02:01:57 -0500

    Adding conditional checks around the download of Terraform assets in the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,11 +9,19 @@ apt-get install -y certbot
 rm -rf /var/lib/apt/lists/*
 
 TERRAFORM_VERSION=0.12.19
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip 2>/dev/null
-unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin
+
+if [ ! -f /usr/local/bin/terraform ]
+then
+    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip 2>/dev/null
+    unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin
+fi
 
 mkdir -p ~/.terraform.d/plugins
 
 TERRAFORM_AWS_VERSION=2.45.0
-wget https://releases.hashicorp.com/terraform-provider-aws/${TERRAFORM_AWS_VERSION}/terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip 2>/dev/null
-unzip -o terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip -d ~/.terraform.d/plugins
+
+if ! ls ~/.terraform.d/plugins/terraform-provider-aws_v${TERRAFORM_AWS_VERSION}_* 1>/dev/null 2>&1
+then
+    wget https://releases.hashicorp.com/terraform-provider-aws/${TERRAFORM_AWS_VERSION}/terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip 2>/dev/null
+    unzip -o terraform-provider-aws_${TERRAFORM_AWS_VERSION}_linux_amd64.zip -d ~/.terraform.d/plugins
+fi
