d30a4a63dbbfa7769e95e20738b816ee720f7b28
Adrian Webb <adrian@webb.sh>
2020-05-08 09:31:01 -0400

    Adding a initialize_terraform method override with listeners generation in the EC2 LB server provider.

--- plugins/server/ec2_lb.py
+++ plugins/server/ec2_lb.py
@@ -2,4 +2,13 @@ from plugins.server import ec2
 
 
 class Provider(ec2.Provider):
-    pass
+
+    def initialize_terraform(self, instance, created):
+        super().initialize_terraform(instance, created)
+
+        relations = self.command.get_relations(instance.facade)
+        instance.config['listeners'] = self.get_instance_values(
+            relations['load_balancer_listeners'],
+            instance.load_balancer_listeners,
+            self.command._load_balancer_listener
+        )
