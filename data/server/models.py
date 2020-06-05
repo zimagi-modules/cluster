c50e688d8d701c0de3b225fcd0c1bcc00ed3c319
Adrian Webb <adrian@webb.sh>
2020-06-05 14:37:39 -0400

    Moving all module model definitions into specification files.

--- data/server/models.py
+++ data/server/models.py
@@ -1,18 +1,8 @@
-from django.db import models as django
-
-from settings.roles import Roles
-from data.load_balancer_listener.models import LoadBalancerListener
-from systems.models import fields, subnet, firewall, provider, load_balancer, group, domain
+from systems.models.index import Model, ModelFacade
 
 
-class ServerFacade(
-    provider.ProviderModelFacadeMixin,
-    group.GroupModelFacadeMixin,
-    firewall.FirewallModelFacadeMixin,
-    domain.DomainModelFacadeMixin,
-    load_balancer.LoadBalancerModelFacadeMixin,
-    subnet.SubnetModelFacadeMixin
-):
+class ServerFacade(ModelFacade('server')):
+
     def get_field_password_display(self, instance, value, short):
         if not value:
             return None
@@ -35,41 +25,10 @@ class ServerFacade(
         return self.error_color(value)
 
 
-class Server(
-    provider.ProviderMixin,
-    group.GroupMixin,
-    firewall.FirewallRelationMixin,
-    domain.DomainMixin,
-    load_balancer.LoadBalancerMixin,
-    subnet.SubnetModel
-):
+class Server(Model('server')):
+
     STATUS_RUNNING = 'running'
     STATUS_UNREACHABLE = 'unreachable'
-
-    public_ip = django.CharField(null = True, max_length = 128)
-    private_ip = django.CharField(null = True, max_length = 128)
-    ssh_port = django.IntegerField(default = 22)
-    user = django.CharField(null = True, max_length = 128)
-    password = fields.EncryptedCharField(null = True, max_length = 1096)
-    private_key = fields.EncryptedDataField(null = True)
-
-    domain_name = django.CharField(null = True, max_length = 128)
-
-    load_balancer_listeners = django.ManyToManyField(LoadBalancerListener,
-        related_name = "%(class)s_relations",
-        editable = False
-    )
-    class Meta(subnet.SubnetModel.Meta):
-        verbose_name = "server"
-        verbose_name_plural = "servers"
-        facade_class = ServerFacade
-        relation = ['domain', 'load_balancer']
-        dynamic_fields = ['status']
-        ordering = ['name']
-        provider_name = 'server'
-
-    def __str__(self):
-        return "{} ({})".format(self.name, self.ip)
 
     @property
     def ip(self):
@@ -78,10 +37,6 @@ class Server(
     @property
     def status(self):
         return self.STATUS_RUNNING if self.ping() else self.STATUS_UNREACHABLE
-
-
-    def allowed_groups(self):
-        return [ Roles.admin, Roles.server_admin ]
 
     def running(self):
         if self.status == self.STATUS_RUNNING:
--- data/server_volume/models.py
+++ data/server_volume/models.py
@@ -1,40 +1,7 @@
-from django.db import models as django
-
-from settings.roles import Roles
-from systems.models import fields, server, provider
+from systems.models.index import Model, ModelFacade
 
 
-class ServerVolumeFacade(
-    provider.ProviderModelFacadeMixin,
-    server.ServerModelFacadeMixin
-):
-    pass
-
-
-class ServerVolume(
-    provider.ProviderMixin,
-    server.ServerModel
-):
-    location = django.CharField(max_length = 256)
-    type = django.CharField(default = 'ext4', max_length = 128)
-    owner = django.CharField(default = 'root', max_length = 128)
-    group = django.CharField(default = 'root', max_length = 128)
-    mode = django.CharField(default = '0755', max_length = 25)
-    options = fields.CSVField()
-
-    class Meta(server.ServerModel.Meta):
-        verbose_name = "server volume"
-        verbose_name_plural = "server volumes"
-        facade_class = ServerVolumeFacade
-        scope_process = 'post'
-        ordering = ['name']
-        provider_name = 'server_volume'
-
-    def __str__(self):
-        return "{} ({})".format(self.server.name, self.name)
-
-    def allowed_groups(self):
-        return [ Roles.admin, Roles.server_admin ]
+class ServerVolume(Model('server_volume')):
 
     def save(self, *args, **kwargs):
         if not isinstance(self.mode, str):
