7443a142f98c4c12cd6d60730cd7ba8d2cc7efb0
Adrian Webb <adrian@webb.sh>
2020-07-12 00:48:21 -0400

    Adding servers dynamic field implementation in the load balancer listener model.

--- data/load_balancer_listener/models.py
+++ data/load_balancer_listener/models.py
@@ -1,1 +1,14 @@
+from systems.models.base import model_index
 from systems.models.index import Model, ModelFacade
+from utility.data import ensure_list
+
+
+class LoadBalancerListener(Model('load_balancer_listener')):
+
+    @property
+    def servers(self):
+        server_facade = model_index().get_facade_index()['server']
+        return list(server_facade.filter(
+            groups__name__in = ensure_list(self.server_groups),
+            subnet__in = list(self.load_balancer.subnets.all())
+        ))
