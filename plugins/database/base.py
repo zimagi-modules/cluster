b3842e6b9e52a9a50b4b0ce42095aef07f3b5923
Adrian Webb <adrian@webb.sh>
2020-10-10 21:36:29 -0400

    Adding an initial database base plgin definition.

---
+++ plugins/database/base.py
@@ -0,0 +1,55 @@
+from systems.plugins.index import BasePlugin
+
+
+class BaseProvider(BasePlugin('database')):
+
+    def prepare_instance(self, instance, created):
+        self.update_domain_reference(instance)
+
+    def finalize_terraform(self, instance):
+        self.remove_domain_reference(instance)
+        super().finalize_terraform(instance)
+
+
+    def update_domain_reference(self, instance):
+        if instance.domain and instance.host:
+            domain_target = "{}.{}".format(
+                instance.domain_name if instance.domain_name else instance.name,
+                instance.domain.name
+            )
+            domain_name = "{}-{}".format(domain_target, instance.host)
+
+            record = self.command._domain_record.retrieve(
+                domain_name,
+                domain = instance.domain
+            )
+            if not record:
+                provider = self.command.get_provider(
+                    self.command._domain_record.meta.provider_name,
+                    instance.domain.provider_type
+                )
+                record = provider.create(domain_name, {
+                    'domain': instance.domain,
+                    'target': domain_target,
+                    'type': 'CNAME',
+                    'values': [ instance.host ]
+                })
+            else:
+                record.initialize(self.command)
+                record.provider.update()
+
+    def remove_domain_reference(self, instance):
+        if instance.domain and instance.host:
+            domain_target = "{}.{}".format(
+                instance.domain_name if instance.domain_name else instance.name,
+                instance.domain.name
+            )
+            domain_name = "{}-{}".format(domain_target, instance.host)
+
+            record = self.command._domain_record.retrieve(
+                domain_name,
+                domain = instance.domain
+            )
+            if record:
+                record.initialize(self.command)
+                record.provider.delete()
