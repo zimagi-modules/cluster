389610f60d1e2d1f62bd4c24fcb15b552b3d0cc6
Adrian Webb <adrian@webb.sh>
2020-10-12 21:07:42 -0400

    Updating the implementation of the deploy command.

--- commands/deploy.py
+++ commands/deploy.py
@@ -4,26 +4,13 @@ from systems.commands.index import Command
 class Deploy(Command('deploy')):
 
     def exec(self):
-
-        def deploy_env(env):
-            self.data("Deploying environment to", str(env))
-            self.exec_remote(env, 'db push')
-
-        self.run_list(
-            self._update_environments(self.server_instances),
-            deploy_env
+        env = self.create_env(self.destination,
+            host = self.destination
         )
-        if self.destination:
-            self.update_env_host(
-                name = self.destination_name,
-                host = self.destination
-            )
+        self.data("Deploying environment to", str(env))
+        self.exec_remote(env, 'db push')
 
-    def _update_environments(self, servers):
-        env_name = self._environment.get_env()
-        environments = []
-        for index, server in enumerate(servers):
-            environments.append(self.create_env("{}-{}".format(env_name, index),
-                host = server.ip
-            ))
-        return environments
+        self.update_env_host(
+            name = self.destination_name,
+            host = self.destination
+        )
