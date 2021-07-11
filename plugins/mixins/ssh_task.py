d26b1d2f60bb33e319d8d3b2efe574290c39b066
Adrian Webb <adrian@webb.sh>
2021-07-10 21:03:19 -0400

    Adding interpolation to the server query in the SSH task plugin mixin.

--- plugins/mixins/ssh_task.py
+++ plugins/mixins/ssh_task.py
@@ -6,7 +6,7 @@ class SSHTaskMixin(ProviderMixin('ssh_task')):
     def _ssh_servers(self, params):
         return self.command.search_instances(
             self.manager.index.get_facade_index()['server'],
-            queries = params.get('servers', self.field_servers),
+            queries = params.get('servers', self._interpolate(self.field_servers, params)),
             joiner = params.get('filter', self.field_filter).upper(),
             error_on_empty = False
         )
