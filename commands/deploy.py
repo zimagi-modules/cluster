0de69b0a8855bfb2934d60b54af4a9261406f143
Adrian Webb <adrian@webb.sh>
2021-07-13 01:45:42 -0400

    Updating the deploy command to use the host_save method when deploying the site to a remote.

--- commands/deploy.py
+++ commands/deploy.py
@@ -4,13 +4,9 @@ from systems.commands.index import Command
 class Deploy(Command('deploy')):
 
     def exec(self):
-        env = self.create_env(self.destination,
-            host = self.destination
-        )
-        self.data("Deploying environment to", str(env))
-        self.exec_remote(env, 'db push')
-
-        self.update_env_host(
+        host = self.save_host(
             name = self.destination_name,
             host = self.destination
         )
+        self.data("Deploying environment to", str(host))
+        self.exec_remote(host, 'db push')
